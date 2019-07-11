50c41e3bd2e54bc496dcbd86336dcc976c68e000
Quentin <quentin.pautrat@gmail.com>
2019-07-11 17:59:47 +0200

    refactor: Remove use of deprecation class.

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -2,8 +2,8 @@
 
 namespace QP\WoohoolabsYinBundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
 {
