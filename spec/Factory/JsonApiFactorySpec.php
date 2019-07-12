5f1ed95f1ac2e6f63626a111be21231d04195985
Quentin Pautrat <quentin.pautrat@gmail.com>
2019-07-12 16:24:31 +0200

    Remove ExpressionLanguage dependency. (#13)

--- spec/Factory/JsonApiFactorySpec.php
+++ spec/Factory/JsonApiFactorySpec.php
@@ -7,14 +7,16 @@ use Psr\Http\Message\ServerRequestInterface;
 use QP\WoohoolabsYinBundle\Factory\JsonApiFactory;
 use Symfony\Bridge\PsrHttpMessage\Factory\DiactorosFactory;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use WoohooLabs\Yin\JsonApi\JsonApi;
 use WoohooLabs\Yin\JsonApi\Exception\ExceptionFactoryInterface;
 
 class JsonApiFactorySpec extends ObjectBehavior
 {
-    public function let(DiactorosFactory $psr7Factory, Request $request, ServerRequestInterface $psrRequest, ExceptionFactoryInterface $exceptionFactory)
+    public function let(DiactorosFactory $psr7Factory, RequestStack $requestStack, Request $request, ServerRequestInterface $psrRequest, ExceptionFactoryInterface $exceptionFactory)
     {
         $this->beConstructedWith($psr7Factory, $exceptionFactory);
+        $requestStack->getCurrentRequest()->willReturn($request);
         $psr7Factory->createRequest($request)->willReturn($psrRequest);
     }
 
@@ -23,10 +25,10 @@ class JsonApiFactorySpec extends ObjectBehavior
         $this->shouldHaveType(JsonApiFactory::class);
     }
 
-    public function it_creates_jsonapi($request, $psr7Factory, $exceptionFactory)
+    public function it_creates_jsonapi($requestStack, $request, $psr7Factory, $exceptionFactory)
     {
         $psr7Factory->createRequest($request)->shouldBeCalled();
-        $this->create($request)->shouldReturnAnInstanceOf(JsonApi::class);
-        $this->create($request)->getExceptionFactory()->shouldReturn($exceptionFactory);
+        $this->create($requestStack)->shouldReturnAnInstanceOf(JsonApi::class);
+        $this->create($requestStack)->getExceptionFactory()->shouldReturn($exceptionFactory);
     }
 }
